8319e00e309ae63a1edea570b9081e547f03dcf3
christopherchay <chay.c2.christopher@gmail.com>
2017-05-08 20:18:43 -0400

    C2: Made tremendous progress in my program here. A new idea for writing also sprouted in my brain too.

--- c2/python/dp.py
+++ c2/python/dp.py
@@ -1,30 +1,33 @@
 import random, time, sys
 
+
+## Changing things: grub -> virus
+
 armour = {'head': {'none': {'name': 'none',
                             'armour': 0,
                             'bprice': 0,
                             'sprice': 0},
-                   'cloth cap': {'name': 'cloth cap',
+                   'byte cap': {'name': 'byte cap',
                             'armour': 2,
                             'bprice': 10,
                             'sprice': 8},
-                   'leather helmet': {'name': 'leather helmet',
+                   'kilobyte helmet': {'name': 'kilobyte helmet',
                             'armour': 4,
                             'bprice': 50,
                             'sprice': 40},
-                   'bronze helmet': {'name': 'bronze helmet',
+                   'megabyte helmet': {'name': 'megabyte helmet',
                             'armour': 8,
                             'bprice': 250,
                             'sprice': 200},
-                   'iron helmet': {'name': 'iron helmet',
+                   'gigabyte helmet': {'name': 'gigabyte helmet',
                             'armour': 16,
                             'bprice': 1250,
                             'sprice': 1000},
-                   'chain coif': {'name': 'chain coif',
+                   'terrabyte coif': {'name': 'terrabyte coif',
                             'armour': 32,
                             'bprice': 6250,
                             'sprice': 5000},
-                   'steel helmet': {'name': 'steel helmet',
+                   'technobyte helmet': {'name': 'technobyte helmet',
                             'armour': 64,
                             'bprice': 31250,
                             'sprice': 25000}},
@@ -33,27 +36,27 @@ armour = {'head': {'none': {'name': 'none',
                             'armour': 2,
                             'bprice': 25,
                             'sprice': 20},
-                    'cloth shirt': {'name': 'cloth shirt',
+                    'byte shirt': {'name': 'byte shirt',
                             'armour': 4,
                             'bprice': 125,
                             'sprice': 100},
-                    'leather vest': {'name': 'leather vest',
+                    'kilobyte vest': {'name': 'kilobyte vest',
                             'armour': 8,
                             'bprice': 625,
                             'sprice': 500},
-                    'bronze chest plate': {'name': 'bronze shirt',
+                    'megabyte chest plate': {'name': 'megabyte shirt',
                             'armour': 16,
                             'bprice': 3125,
                             'sprice': 2500},
-                    'iron platemail': {'name': 'iron platemail',
+                    'gigabyte platemail': {'name': 'gigabyte platemail',
                             'armour': 32,
                             'bprice': 15625,
                             'sprice': 12500},
-                    'chainmail shirt': {'name': 'chainmail shirt',
+                    'terrabyte shirt': {'name': 'terrabyte shirt',
                             'armour': 64,
                             'bprice': 78125,
                             'sprice': 62500},
-                    'steel ringmail': {'name': 'steel ringmail',
+                    'technobyte ringmail': {'name': 'technobyte ringmail',
                             'armour': 128,
                             'bprice': 390620,
                             'sprice': 312500}},
@@ -62,27 +65,27 @@ armour = {'head': {'none': {'name': 'none',
                             'armour': 0,
                             'bprice': 0,
                             'sprice': 0},
-                    'cloth leggings': {'name': 'cloth leggings',
+                    'byte pants': {'name': 'byte pants',
                             'armour': 3,
                             'bprice': 15,
                             'sprice': 12},
-                    'leather pants': {'name': 'leather pants',
+                    'kilobyte pants': {'name': 'kilobyte pants',
                             'armour': 6,
                             'bprice': 75,
                             'sprice': 60},
-                    'bronze greaves': {'name': 'bronze greaves',
+                    'megabyte greaves': {'name': 'megabyte greaves',
                             'armour': 12,
                             'bprice': 375,
                             'sprice': 300},
-                    'iron pants': {'name': 'iron pants',
+                    'gigabyte pants': {'name': 'gigabyte pants',
                             'armour': 24,
                             'bprice': 1875,
                             'sprice': 1500},
-                    'chainmail pants': {'name': 'chainmail pants',
+                    'terrabyte pants': {'name': 'terrabyte pants',
                             'armour': 48,
                             'bprice': 9375,
                             'sprice': 7500},
-                    'steel pants': {'name': 'steel pants',
+                    'technobyte pants': {'name': 'technobyte pants',
                             'armour': 96,
                             'bprice': 46875,
                             'sprice': 37500}},
@@ -91,27 +94,27 @@ armour = {'head': {'none': {'name': 'none',
                             'armour': 0,
                             'bprice': 0,
                             'sprice': 0},
-                    'cloth shoes': {'name': 'cloth shoes',
+                    'byte shoes': {'name': 'byte shoes',
                             'armour': 1,
                             'bprice': 5,
                             'sprice': 4},
-                    'leather moccasins': {'name': 'leather moccasins',
+                    'kilobyte moccasins': {'name': 'kilobyte moccasins',
                             'armour': 2,
                             'bprice': 25,
                             'sprice': 10},
-                    'bronze boots': {'name': 'bronze boots',
+                    'megabyte boots': {'name': 'megabyte boots',
                             'armour': 4,
                             'bprice': 125,
                             'sprice': 100},
-                    'iron boots': {'name': 'iron boots',
+                    'gigabyte boots': {'name': 'gigabyte boots',
                             'armour': 8,
                             'bprice': 625,
                             'sprice': 500},
-                    'chainmail socks': {'name': 'chainmail socks',
+                    'terrabyte socks': {'name': 'terrabyte socks',
                             'armour': 16,
                             'bprice': 3125,
                             'sprice': 2500},
-                    'steel boots': {'name': 'steel boots',
+                    'technobyte boots': {'name': 'technobyte boots',
                             'armour': 32,
                             'bprice': 15625,
                             'sprice': 12500}},
@@ -120,27 +123,27 @@ armour = {'head': {'none': {'name': 'none',
                             'armour': 0,
                             'bprice': 0,
                             'sprice': 0},
-                    'cloth gloves': {'name': 'cloth gloves',
+                    'byte gloves': {'name': 'byte gloves',
                             'armour': 1,
                             'bprice': 5,
                             'sprice': 4},
-                    'leather gloves': {'name': 'leather gloves',
+                    'kilobyte gloves': {'name': 'kilobyte gloves',
                             'armour': 2,
                             'bprice': 125,
                             'sprice': 100},
-                    'bronze gloves': {'name': 'bronze gloves',
+                    'megabyte gloves': {'name': 'megabyte gloves',
                             'armour': 4,
                             'bprice': 625,
                             'sprice': 500},
-                    'iron gloves': {'name': 'iron gloves',
+                    'gigabyte gloves': {'name': 'gigabyte gloves',
                             'armour': 8,
                             'bprice': 3125,
                             'sprice': 2500},
-                    'chainmail gloves': {'name': 'chainmail gloves',
+                    'terrabyte gloves': {'name': 'terrabyte gloves',
                             'armour': 16,
                             'bprice': 15625,
                             'sprice': 12500},
-                    'steel gloves': {'name': 'steel gloves',
+                    'technobyte gloves': {'name': 'technobyte gloves',
                             'armour': 32,
                             'bprice': 78125,
                             'sprice': 62500}}}
@@ -151,6 +154,7 @@ class player:
       self.hp = 75
       self.level = 1
       self.xp = 0
+      self.bit = 100
 
       self.level_cost = {2: 10000,
                          3: 50000,
@@ -161,12 +165,15 @@ class player:
       self.attributes = {'strength': 1,
                          'agility': 1}
 
-      self.skills = {'dodge': {'level': 1},
-                     'slash': {'damage': 3, 'level': 1}}
+      self.skills = {'dodge': {'level': 1, 'cost': 5},
+                     'slash': {'damage': 10, 'level': 1, 'cost': 5}}
+                     #Slash skill damage increases by 50% every level
+                     #Dodge and slash skill cost increases 
+      self.atttype = 'slashed'
 
-      self.armour = {'head': armour['head']['cloth cap'],
-                     'torso': armour['torso']['cloth shirt'],
-                     'legs': armour['legs']['cloth leggings'],
+      self.armour = {'head': armour['head']['none'],
+                     'torso': armour['torso']['byte shirt'],
+                     'legs': armour['legs']['byte pants'],
                      'feet': armour['feet']['none'],
                      'hands': armour['hands']['none']}
 
@@ -174,9 +181,9 @@ class player:
                          + self.armour['feet']['armour'] + self.armour['hands']['armour']
 
       
-      self.inventory = []
+      self.inventory = {}
 
-      self.weapon = 'knife'
+      self.weapon = 'technoblade'
 
       
    def getHurt(self, damage):
@@ -209,7 +216,7 @@ class player:
 
    def unequipArmour(self, local, item):
       self.resistance -= self.armour[local]['armour']
-      self.inventory.append(self.armour[local]['name'])
+      self.inventory = (self.armour[local]['name'])
       self.armour[local] = armour[local]['none']
 
 
@@ -221,12 +228,18 @@ class player:
       return 'Congradulations!! {} has just leveled up!! Good work.' .format(self.name)
 
 
+   def displayInv(self):
+      for i in self.inventory:
+         print(i['name'])
+      
+
    def __str__(self):
       return '''
 Name: {}
    HP: {}
    Level: {}
    XP: {}/{}
+   Bits: {}
 
 Attributes:
    Strength: {}
@@ -246,7 +259,7 @@ Armour:
       Legs: {}
       Feet: {}
       Hands: {}
-''' .format(self.name, self.hp, self.level, self.xp, self.level_cost[self.level+1], self.attributes['strength'],
+''' .format(self.name, self.hp, self.level, self.xp, self.level_cost[self.level+1], self.bit, self.attributes['strength'],
             self.attributes['agility'], self.skills['dodge']['level'], self.skills['slash']['level'], self.weapon,
             self.skills['slash']['damage'], self.resistance, self.armour['head']['name'], self.armour['torso']['name'],
             self.armour['legs']['name'], self.armour['feet']['name'], self.armour['hands']['name'])
@@ -255,3 +268,109 @@ Armour:
 
 dp = player()
 print(dp)
+
+class virus():
+   def __init__(self):
+      self.hp = 50
+      self.name = 'virus'
+      self.damage = 10
+      self.strength = 1
+      self.agility = 1
+
+      self.resistance = 5
+
+      self.skills = {'dodge': {'level': 1},
+                     'smash': {'damage': 7, 'level': 1}}
+      self.atttype = 'smashed'
+
+      self.xp = 10
+      self.bit = random.randint(5, 20)
+
+
+
+
+
+class battle:
+   def fight(self, a, d):
+      while a.hp > 0 and d.hp > 0:
+         print('{}: {}' .format(a.name, a.hp))
+         print('{}: {}' .format(d.name, d.hp))
+         
+         battle().turn(a, d, 'slash')
+         battle().turn(d, a, 'smash')
+         print('^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^')
+         input('<press enter to continue>')
+
+      else:
+         if a.hp <= 0 and d.hp <= 0:
+            print('{} and {} have died from their numerous wounds.' .format(a.name, d.name))
+
+         elif a.hp <= 0:
+            print('{} has been killed.' .format(a.name))
+
+         elif d.hp <= 0:
+            print('{} has been killed.' .format(d.name))
+            a.bit += d.bit
+            a.xp += d.xp
+            
+      
+
+   def turn(self, a, d, skill):
+      if skill == 'slash':
+         damagetype = 'cutting'
+      else:
+         damagetype = 'crushing'
+      
+      attackchance = random.randint(1, a.skills[skill]['level']) + random.randint(1, 6)
+      defendchance = random.randint(1, a.skills['dodge']['level']) + random.randint(1, 6)
+      if attackchance > defendchance:
+         damagedealt = random.randint(1, int(a.skills[skill]['damage']))
+         damagedealt += random.randint(0, 6)
+         damagedealt -= d.resistance
+         if damagedealt <= 0:
+            damagedealt = 0
+         print('{} {} {} for {} {} damage!!!!' .format(a.name, a.atttype, d.name, damagetype, damagedealt))
+         d.hp -= damagedealt
+
+      else:
+         print('{} missed!' .format(a.name))
+
+
+class technovillage:
+   def signpost(self):
+      print('You find yourself at a sign post at the center of Technovillage. Where\'d you like to go?')
+   
+   def fileinn(self):
+      # You can rest and restore your hp here
+      price = 10 + 0.1*dp.level
+      print('"Good reboot to you! Fileinn at your service! We\'ve got the most comfortable beds in all Computa!"')
+      print('"Would you like a room? It only costs {} bits and all your hp gets recovered."' .format(price))
+      print('You can type "yes" to sleep or "no" to leave.')
+      action = input('Answer:')
+
+      if action == 'yes':
+         if dp.bit >= price:
+            print('You sleep in an extremely comfortable bed and wake up bright and early the next day.')
+            print('"Good reboot! Hope you slept well."')
+         else:
+            print('Uhoh, looks like you don\'t have enough bits. Talk to Lena and she will heal you for half price.')
+      elif action == "no":
+         print('Aww-right, come again soon!!')
+      else:
+         return technovillage().fileinn()
+
+   def downloadstore(self):
+      #This is where you buy and sell items
+      print('Hey, welcome to the download store! download any armour you like!')
+      for i in armour:
+         for x in armour[i]:
+            if x == 'none':
+               continue
+            print('{}: Cost: {}, Armour: {}' .format(armour[i][x]['name'], armour[i][x]['bprice'], armour[i][x]['armour']))
+
+   def trashbin(self):
+      #Fighting area
+      print('Welcome to the trashbin. It\'s endless and you can fight virtually anything.')
+
+
+technovillage().signpost()
