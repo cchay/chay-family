689c693c70d6e8812000ab40ab27b68a9bb23eb3
Caleb Chay <chay.c3.caleb@gmail.com>
2017-07-26 20:12:19 -0400

    C3: Removed Hello World Anime

--- C3/mozartsArcade.py
+++ C3/mozartsArcade.py
@@ -12,10 +12,9 @@ print('''You look at a rather scanty list of games:
                Z
                Adventure
                Addition
-               Hello World Anime
 (please enter the name of the game just the way it is written.)''')
 
-print( '(For your information, "Hello World Anime" only works if you have Pygame installed on your machine.)' )
+
 
 game = input()
 if game == "Z":
@@ -223,80 +222,6 @@ elif game == "Addition":
       print('Correct!')
    else:
       print('Nope! The answer is  ' + str(number1 + number2))
-
-
-
-############################################################################
-
-
-
-
-
-elif game == "Hello World Anime":
-        import random, sys, time, math, pygame
-        from pygame.locals import *
-
-        #set up pygame
-        pygame.init()
-
-        #set up window
-        windowSurface = pygame.display.set_mode((500, 400), 0, 32)
-        pygame.display.set_caption('Hello World!')
-
-        #set up colors
-        BLACK = (0, 0, 0)
-        WHITE = (225, 225, 225)
-        RED = (225, 0, 0)
-        GREEN = (0, 225, 0)
-        BLUE = (0, 0, 225)
-
-        #set up fonts
-        basicFont = pygame.font.SysFont(None, 48)
-
-        #set up the text
-        text = basicFont.render('Hello World!', True, WHITE, BLUE)
-        textRect = text.get_rect()
-        textRect.centerx = windowSurface.get_rect().centerx
-        textRect.centery = windowSurface.get_rect().centery
-
-        #draw the white background onto the surface
-        windowSurface.fill(WHITE)
-
-        #draw a green polygon onto the surface
-        pygame.draw.polygon(windowSurface, GREEN, ((146, 0), (291,106), (236, 277), (56, 277), (0, 106)))
-        
-        #draw some blue lines onto the surface
-        pygame.draw.line(windowSurface, BLUE, (60, 60), (120, 60), 4)
-        pygame.draw.line(windowSurface, BLUE, (120, 60), (60, 120))
-        pygame.draw.line(windowSurface, BLUE, (60,120), (120, 120), 4)
-
-        #draw a blue circle onto the surface
-        pygame.draw.circle(windowSurface, BLUE, (300, 50), 20, 0)
-
-        #draw a red ellipse onto the surface
-        pygame.draw.ellipse(windowSurface, RED, (300, 250, 40, 80), 1)
-
-        #draw the text's background rectangle onto the surface
-        pygame.draw.rect(windowSurface, RED, (textRect.left - 20, textRect.bottom - 20, textRect.width + 40, textRect.height + 40))
-
-        #get a pixel array of the surface
-        pixArray = pygame.PixelArray(windowSurface)
-        pixArray[480][380] = BLACK
-        del pixArray
-
-        #draw the text onto the surface
-        windowSurface.blit(text, textRect)
-
-        #draw the window onto the surface
-        pygame.display.update()
-
-        #run the game loop
-        while True:
-            for event in pygame.event.get():
-                if event.type ==QUIT:
-                    pygame.quit()
-                    sys.exit()
-
 
 
 
