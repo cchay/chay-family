d8615b13a705e40237a61bd1db9ee6e7b61b0783
Chris Chay <chay.c0.chris@gmail.com>
2017-12-08 16:39:46 -0500

    master: c0, house cleaning; removing all folders & files from master

--- c2/python/practice/test.py
+++
@@ -1,8 +0,0 @@
-entry_number = 1
-
-file = open('prac.py', 'w')
-file.write('Top Scores:\n\nName:\tScore:\nChris\t1234 pts')
-file.close()
-file = open('prac.py', 'r')
-print(file.read())
-file.close()
--- c2/python/practice/tutor/input_output.py
+++
@@ -1,9 +0,0 @@
-## What is input?
-## What is output?
-
-name = input('Type name here: ')
-age = input('Type age here: ')
-
-print(name, age)
-
-# Create a program that receives three of my favourite superheroes and output it
